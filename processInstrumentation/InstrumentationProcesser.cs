8c7d5c6e7de8b7a933cdd1bfc98f0d6ef3b40043
Vince Dániel <wynnid645@gmail.com>
2017-05-12 12:10:30 +0200

    Move row processing logic to separated function. Add exception handling.

--- processInstrumentation/InstrumentationProcesser.cs
+++ processInstrumentation/InstrumentationProcesser.cs
@@ -17,86 +17,19 @@ namespace processInstrumentation
 
             using (var reader = new StreamReader(fileStream))
             {
-                string row, key;
+                string row;
                 while ((row = reader.ReadLine()) != null)
                 {
-                    var columns = row.Split(' ');
                     try
                     {
-                        key = string.Concat(columns[1], " ", columns[2]);
+                        ProcessRow(functions, row);
                     }
-                    catch (Exception) { continue; }
-
-                    switch (columns[0])
+                    catch (Exception e)
                     {
-                        // "e 0x2518de0 0x306e4b6 1494409547 824698315"
-                        case "e":
-                            var enumerable = functions.Where(x => x.Key.Equals(key));
-                            if (enumerable.Any())
-                            {
-                                OutputTime time;
-                                time.Secundum = ulong.Parse(columns[3]);
-                                time.NanoSecundum = ulong.Parse(columns[4]);
-
-                                enumerable.First().Value.Stack.Push(time);
-                            }
-                            else
-                            {
-                                var temp = new InstrumentRowType();
-                                OutputTime time;
-                                time.Secundum = ulong.Parse(columns[3]);
-                                time.NanoSecundum = ulong.Parse(columns[4]);
-                                temp.Stack.Push(time);
-
-                                functions.Add(new KeyValuePair<string, InstrumentRowType>(key, temp));
-                            }
-                            break;
-                        // x 0x2518de0 0x306e4b6 1494409547 824777303
-                        case "x":
-                            var iEnumerable = functions.Where(x => x.Key.Equals(key));
-                            if (iEnumerable.Any())
-                            {
-                                var temp = iEnumerable.First();
-                                temp.Value.Count++;
-
-                                OutputTime time;
-                                if (temp.Value.Stack.Any())
-                                    time = temp.Value.Stack.Pop();
-                                else
-                                    continue;
-
-
-                                ulong nanoSeconds;
-                                ulong seconds = ulong.Parse(columns[3]) - time.Secundum;
-                                ulong currentNs = ulong.Parse(columns[4]);
-
-                                if (currentNs > time.NanoSecundum)
-                                {
-                                    nanoSeconds = currentNs - time.NanoSecundum;
-                                }
-                                else
-                                {
-                                    nanoSeconds = (999999999 - time.NanoSecundum) + currentNs;
-                                }
-
-                                ulong overflowCheck = temp.Value.Summary.NanoSecundum + nanoSeconds;
-                                if (overflowCheck > 999999999)
-                                {
-                                    temp.Value.Summary.Secundum += seconds + 1;
-                                    temp.Value.Summary.NanoSecundum = overflowCheck - 999999999;
-                                }
-                                else
-                                {
-                                    temp.Value.Summary.NanoSecundum += nanoSeconds;
-                                    temp.Value.Summary.Secundum += seconds;
-                                }
-                            }
-
-                            break;
-                        default:
-                            continue;
+                        Console.WriteLine(string.Concat("An exception has thrown: ", e.Message,
+                            " Row: ", row));
                     }
-                }
+                } 
             }
             return functions;
         }
@@ -120,6 +53,73 @@ namespace processInstrumentation
                 normalizedResults.Add(element);
             }
             return normalizedResults;
+        }
+
+        private static void ProcessRow(ICollection<KeyValuePair<string, InstrumentRowType>> functions, string row)
+        {
+            var columns = row.Split(' ');
+            string key = string.Concat(columns[1], " ", columns[2]);
+
+            switch (columns[0])
+            {
+                // "e 0x2518de0 0x306e4b6 1494409547 824698315"
+                case "e":
+                    var enumerable = functions.Where(x => x.Key.Equals(key));
+                    if (enumerable.Any())
+                    {
+                        OutputTime time;
+                        time.Secundum = ulong.Parse(columns[3]);
+                        time.NanoSecundum = ulong.Parse(columns[4]);
+
+                        enumerable.First().Value.Stack.Push(time);
+                    }
+                    else
+                    {
+                        var temp = new InstrumentRowType();
+                        OutputTime time;
+                        time.Secundum = ulong.Parse(columns[3]);
+                        time.NanoSecundum = ulong.Parse(columns[4]);
+                        temp.Stack.Push(time);
+
+                        functions.Add(new KeyValuePair<string, InstrumentRowType>(key, temp));
+                    }
+                    break;
+                // x 0x2518de0 0x306e4b6 1494409547 824777303
+                case "x":
+                    var iEnumerable = functions.Where(x => x.Key.Equals(key));
+                    if (iEnumerable.Any())
+                    {
+                        var temp = iEnumerable.First();
+                        var time = temp.Value.Stack.Pop();
+                        temp.Value.Count++;
+
+                        ulong nanoSeconds;
+                        ulong seconds = ulong.Parse(columns[3]) - time.Secundum;
+                        ulong currentNs = ulong.Parse(columns[4]);
+
+                        if (currentNs > time.NanoSecundum)
+                        {
+                            nanoSeconds = currentNs - time.NanoSecundum;
+                        }
+                        else
+                        {
+                            nanoSeconds = (999999999 - time.NanoSecundum) + currentNs;
+                        }
+
+                        ulong overflowCheck = temp.Value.Summary.NanoSecundum + nanoSeconds;
+                        if (overflowCheck > 999999999)
+                        {
+                            temp.Value.Summary.Secundum += seconds + 1;
+                            temp.Value.Summary.NanoSecundum = overflowCheck - 999999999;
+                        }
+                        else
+                        {
+                            temp.Value.Summary.NanoSecundum += nanoSeconds;
+                            temp.Value.Summary.Secundum += seconds;
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
